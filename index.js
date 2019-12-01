e7fe3b920d8c7b3b3d00da95318f8e7c09bec623
Philip Ng <phil__n@hotmail.com>
2019-11-30 22:14:10 -0500

    added tweetjs.js which pulls the most recent tweets from a specified user's id

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
-var counter = 75
+var counter = 75;
 
-function setTime(){
-    var timerInterval = setInterval(function(){
-        if(counter > 0) {
-          counter--;
-          $(".countdown").text(counter);
-          }
-    }, 1000);
+function setTime() {
+  var timerInterval = setInterval(function() {
+    if (counter > 0) {
+      counter--;
+      $(".countdown").text(counter);
+    }
+  }, 1000);
 }
 
-$("start-btn").on("click",function(){
-    //setTime()
-    
-})
+$("#start-btn").on("click", function() {
+  console.log("response");
+  var queryURL = "https://api.twitter.com/1.1/statuses/user_timeline.json";
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {});
+});
 
+TweetJs.ListTweetsOnUserTimeline("MicroBlizz", function(data) {
+  console.log(data);
+});
