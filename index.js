7ba9c8adec121c10914fd124834adef0baf33ee4
Abdiwahab Ahmed <abdiwahabahmed@Abdiwahabs-MacBook-Air.local>
2019-12-03 06:28:17 -0500

    adjusted char limit

--- index.js
+++ index.js
@@ -21,11 +21,11 @@ TweetJs.ListTweetsOnUserTimeline("TheOnion", function(data) {
     getTweets = localStorage.getItem("test")
     filteredTweets = []
     filteredTweets.push(getTweets)
-    allTweets = filteredTweets[0].split(",").filter(x=>x.length>100&&!x.includes("RT" && "@" && "\u2026"))
+    allTweets = filteredTweets[0].split(",").filter(x=>x.length>110&&!x.includes("RT" && "@" && "\u2026"))
 
     var unfilteredTweets2 = []
 
-TweetJs.ListTweetsOnUserTimeline("cnn", function(data) {
+TweetJs.ListTweetsOnUserTimeline("InsideEdition", function(data) {
   for (i = 0; i < data.length; i++){
     var tweetData2 = data[i].text
     unfilteredTweets2.push(tweetData2)
@@ -34,7 +34,7 @@ TweetJs.ListTweetsOnUserTimeline("cnn", function(data) {
     getTweets2 = localStorage.getItem("test")
     filteredTweets2 = []
     filteredTweets2.push(getTweets2)
-    allTweets2 = filteredTweets2[0].split(",").filter(x=>x.length>110&&!x.includes("RT" && "@" && "\u2026"))
+    allTweets2 = filteredTweets2[0].split(",").filter(x=>x.length>90&&!x.includes("RT" && "@" && "\u2026"))
 
 
   var thirdArr = allTweets.concat(allTweets2)
